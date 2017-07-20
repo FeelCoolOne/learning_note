f9fb43fa1974e1465782f80cfcd144f888c98b77
bit <ygh_cs@163.com>
2017-07-20 17:19:51 +0800

    add ndcg

---
+++ ndcg.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+
+def dcg_at_k(r, k):
+    r = np.asfarray(r)[:k]
+    if r.size:
+        return np.sum(np.subtract(np.power(2, r), 1) / np.log2(np.arange(2, r.size + 2)))
+    return 0.
+
+
+def ndcg_at_k(r, k):
+    idcg = dcg_at_k(sorted(r, reverse=True), k)
+    if not idcg:
+        return 0.
+    return dcg_at_k(r, k) / idcg
